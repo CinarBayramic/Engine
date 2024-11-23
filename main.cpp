36ce5037f849c18c22deb19e2dac26dd36be72d4
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2024-11-23 23:12:53 +0300

    space

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//i dont understand legal magic so please dont copy my work unless you are going to improve it and send
+//a pull request on my github project.
+#include <iostream>
+
+int main(int argc, char**argv) {
+
+	std::cin.get();
+}
