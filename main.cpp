afa5844945bb05377f846ed9304326a655ad66c4
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2024-12-08 22:28:57 +0300

    added main file to test and added mesh and intvec types (no initialisers/constructors)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 //i dont understand legal magic so please dont copy my work unless you are going to improve it and send
 //a pull request on my github project.
 #include <iostream>
-
+#include "types.hpp"
+#include "functions.hpp"
 int main(int argc, char**argv) {
-
+	cgl::vec3 v = cgl::vec3(5, 3,1);
+	double aspr = 4 / 3;
+	double fov = 90;
+	double WindowHeight = 720;
+	double WindowWidth = WindowHeight * aspr;
+	std::cout << cgl::perspectiveproject(v,fov,aspr,100,0.01).x;
+	
 	std::cin.get();
 }
--- types.hpp
+++ types.hpp
@@ -1,12 +1,11 @@
+#ifndef TYPES_HPP
 #define TYPES_HPP
-#ifdef TYPES_HPP
 
 namespace cgl {
 	class vec2
 	{
 	public:
 		vec2(double X, double Y);
-		vec2() {}
 		double x, y;
 	};
 
@@ -15,6 +14,16 @@ namespace cgl {
 	public:
 		vec3(double X, double Y, double Z);
 		double x, y, z;
+	};
+	class intvec3 {
+		intvec3(int X, int Y);
+		int x, y, z;
+	};
+	class mesh
+	{
+	public:
+		vec3* verticies;//köþeler (sorry for turkish commentation i will translate later)
+		intvec3* indecies;//köþelere iþaret eden iþaretçiler
 	};
 }
 #endif
