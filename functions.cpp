d5b19ed2620d4cfe9909e98c3324fb286d5ec7de
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2024-12-08 22:05:22 +0300

    mulitply pprojection matrix w/ the vector

--- functions.cpp
+++ functions.cpp
@@ -11,6 +11,15 @@ namespace cgl {
 			0,0,-((far+near)/(far-near)),(2*far*near)/(far-near),
 			0,0,-1,0
 		}; //#TODO implement matrix multiplication for the function to do its thing.
+		double temp[4] = {vec.x,vec.y,vec.z,1.0};
+		double out[4] = {0.0,0.0,0,0.0};
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				out[i] += projectionmatrix[i * 4 + j] * temp[j];
+			}
+		}
+		cgl::vec2 o = cgl::vec2(out[0], out[1]);
+		return o;
 
 	};
 }
